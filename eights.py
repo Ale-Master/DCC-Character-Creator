89edcea30e3a0d86a84cd81e6efd8fd4ffd33b3f
Ale-Master <lemasteraustin7@gmail.com>
2020-12-21 08:39:54 -0600

    Update eights.py

--- eights.py
+++ eights.py
@@ -5,4 +5,8 @@ while count != 2000000:
     n = count 
     digits = [int(x) for x in str(n)]
     count += 1
+    if 8 in digits:
+        total = (total + (count - 1))
+        eights += 1
 print(eights)
+print(total)
